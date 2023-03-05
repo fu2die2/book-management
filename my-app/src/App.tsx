6eeb0dc8aa2b92d990199d70f657fc0118c812f5
shohei.ueno <shohei.ueno@fox-hound.tech>
2023-03-06 02:23:17 +0900

    React-routerの実装

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import RentalPage from './components/pages/RentalPage';
+import NotFound from './components/pages/NotFound';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <h1>Hello React Router v6</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rental" element={<RentalPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    // </div>
   );
 }
 
---
+++ my-app/src/components/pages/RentalPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const RentalPage: React.FC = () => {
+  return (
+    <>
+      <h2>書籍リスト</h2>
+      <Link to="/">TOPに戻る</Link>
+    </>
+  )
+}
+
+export default RentalPage;
